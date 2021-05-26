2b3af095f454bdd69efd9ff83de92575811c8e40
Apeksha Singh <apekshasingh664@gmail.com>
2021-05-26 20:50:14 +0530

    first commit

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem(){
-    return <h2>Expense Item!!!</h2>
+    return 
+        <div>
+        <div>may 26 2021</div>
+        <div>
+            <h2>Car</h2>
+            <div>$234.67</div>
+        </div>
+    </div>
+    
 }
 
 export default ExpenseItem;
