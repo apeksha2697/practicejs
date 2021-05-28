e09d81b57705d396778783fb5994ff03b8584a2d
Apeksha Singh <apekshasingh664@gmail.com>
2021-05-28 12:00:56 +0530

    added expense ans UI folder

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,19 @@
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate.js';
+import Card from '../UI/Card';
+
+function ExpenseItem(props){
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date}></ExpenseDate>
+    <div className="expense-item__description">
+        <h2>{props.item}</h2>
+        <div className="expense-item__price">{props.amount}</div>
+    </div>
+    </Card>
+  );
+        
+    
+}
+
+export default ExpenseItem;
