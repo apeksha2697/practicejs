9c087201ae6048cfe83b54abbe70c28b6b636186
Apeksha Singh <apekshasingh664@gmail.com>
2021-05-31 11:49:09 +0530

    used states to update item name

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,23 @@
+import React, {useState} from 'react';
+
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate.js';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
+  const [item, setTitle] = useState(props.item);
+
+  const clickHandler =() => {
+    setTitle('updated!!');
+    console.log(item);
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
     <div className="expense-item__description">
-        <h2>{props.item}</h2>
+        <h2>{item}</h2>
         <div className="expense-item__price">{props.amount}</div>
+        <button onClick={clickHandler}>Change Title</button>
     </div>
     </Card>
   );
